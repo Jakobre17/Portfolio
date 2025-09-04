cdf65e73824d952d75eae2e8140a9ad9e6f199cb
Jakobre17 <95107266+Jakobre17@users.noreply.github.com>
2025-09-04 18:14:58 -0500

    Upload Space Shooter

---
+++ Galaxy Defiance/enemies/enemy.gd
@@ -0,0 +1,31 @@
+class_name Enemy
+extends Node2D
+
+@onready var stats_component = $StatsComponent
+@onready var move_component = $MoveComponent
+@onready var visible_on_screen_notifier_2d = $VisibleOnScreenNotifier2D
+@onready var scale_component = $ScaleComponent
+@onready var flash_component = $FlashComponent
+@onready var shake_component = $ShakeComponent
+@onready var hurtbox_component = $HurtboxComponent
+@onready var hitbox_component = $HitboxComponent
+@onready var destroyed_component = $DestroyedComponent
+@onready var score_component = $ScoreComponent
+@onready var variable_pitch_audio_stream_player = $VariablePitchAudioStreamPlayer
+
+
+
+func _ready():
+	stats_component.no_health.connect(func():
+		score_component.adjust_score()
+		)
+	
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+		variable_pitch_audio_stream_player.play_with_variance()
+	)
+	stats_component.no_health.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
---
+++ Galaxy Defiance/player_ship/ship.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var left_muzzle: Marker2D = $LeftMuzzle
+@onready var right_muzzle: Marker2D = $RightMuzzle
+@onready var spawner_component: Node2D = $SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var scale_component = $ScaleComponent
+@onready var animated_sprite_2d = $Anchor/AnimatedSprite2D
+@onready var move_component = $MoveComponent
+@onready var flame_animated_sprite = $Anchor/FlameAnimatedSprite
+@onready var variable_pitch_audio_stream_player = $VariablePitchAudioStreamPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fire_rate_timer.timeout.connect(fire_lasers)
+	
+
+func  fire_lasers() -> void:
+	variable_pitch_audio_stream_player.play_with_variance()
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
+
+func _process(delta) -> void:
+	animate_the_ship()
+	
+
+func animate_the_ship() -> void:
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
+	pass
